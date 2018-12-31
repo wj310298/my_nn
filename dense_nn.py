ec58a6ceb0dbe8be6417576ada0ab5731c30759d
Weng Jie <wengjie@winwin-inc.com>
2018-12-31 11:06:25 +0800

    Change dense network

--- dense_nn.py
+++ dense_nn.py
@@ -1,10 +1,17 @@
-from keras import layers, models
+from keras import layers, models, regularizers
 
 x_input = layers.Input((100,))
-hidden1 = layers.Dense(100, activation='relu')(x_input)
-hidden2 = layers.Dense(100, activation='relu')(layers.add([x_input, hidden1]))
-hidden3 = layers.Dense(100, activation='relu')(layers.add([x_input, hidden1, hidden2]))
-y = layers.Dense(100, activation='sigmoid')(layers.add([x_input, hidden1, hidden2, hidden3]))
+hidden1 = layers.Dense(100, kernel_regularizer=regularizers.l2())(x_input)
+hidden1 = layers.BatchNormalization(hidden1)  # I think it's important
+hidden1 = layers.ReLU(hidden1)
+hidden2 = layers.Dense(100, kernel_regularizer=regularizers.l2())(layers.add([x_input, hidden1]))
+hidden2 = layers.BatchNormalization(hidden2)
+hidden2 = layers.ReLU(hidden2)
+hidden3 = layers.Dense(100, kernel_regularizer=regularizers.l2())(layers.add([x_input, hidden1, hidden2]))
+hidden3 = layers.BatchNormalization(hidden3)
+hidden3 = layers.ReLU(hidden3)
+y = layers.Dense(100, activation='sigmoid', kernel_regularizer=regularizers.l2())(
+    layers.add([x_input, hidden1, hidden2, hidden3]))
 
 model = models.Model(x_input, y)
 model.summary()
