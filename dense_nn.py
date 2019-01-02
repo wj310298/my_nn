9d78f23e32ccd4b59cb938a898c8d5735035672a
Weng Jie <wengjie@winwin-inc.com>
2019-01-02 19:29:08 +0800

    fix bugs

--- dense_nn.py
+++ dense_nn.py
@@ -4,14 +4,14 @@ x_input = layers.Input((100,))
 hidden1 = layers.Dense(100, kernel_regularizer=regularizers.l2())(x_input)
 hidden1 = layers.BatchNormalization()(hidden1)  # I think it's important
 hidden1 = layers.ReLU()(hidden1)
-hidden2 = layers.Dense(100, kernel_regularizer=regularizers.l2())(layers.add([x_input, hidden1]))
+hidden2 = layers.Dense(100, kernel_regularizer=regularizers.l2())(layers.concatenate([x_input, hidden1]))
 hidden2 = layers.BatchNormalization()(hidden2)
 hidden2 = layers.ReLU()(hidden2)
-hidden3 = layers.Dense(100, kernel_regularizer=regularizers.l2())(layers.add([x_input, hidden1, hidden2]))
+hidden3 = layers.Dense(100, kernel_regularizer=regularizers.l2())(layers.concatenate([x_input, hidden1, hidden2]))
 hidden3 = layers.BatchNormalization()(hidden3)
 hidden3 = layers.ReLU()(hidden3)
 y = layers.Dense(100, activation='sigmoid', kernel_regularizer=regularizers.l2())(
-    layers.add([x_input, hidden1, hidden2, hidden3]))
+    layers.concatenate([x_input, hidden1, hidden2, hidden3]))
 
 model = models.Model(x_input, y)
 model.summary()
